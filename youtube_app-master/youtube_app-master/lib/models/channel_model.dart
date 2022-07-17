51a17b70af1c1100ba376feb448d017b80b0078d
Ishaan Gupta <71877528+Ishaan108109@users.noreply.github.com>
2022-07-17 17:12:45 +0530

    Add files via upload

---
+++ youtube_app-master/youtube_app-master/lib/models/channel_model.dart
@@ -0,0 +1,44 @@
+import 'video_model.dart';
+
+class Channel {
+  final String id;
+  final String title;
+  final String profilePictureUrl;
+  final String subscriberCount;
+  final String videoCount;
+  final String uploadPlaylistId;
+
+  List<Video> videos;
+
+  Channel({
+    this.id,
+    this.title,
+    this.profilePictureUrl,
+    this.subscriberCount,
+    this.videoCount,
+    this.uploadPlaylistId,
+    this.videos,
+  });
+
+  factory Channel.fromMap(Map<String, dynamic> map) {
+    return Channel(
+      id: map['id'],
+      title: map['snippet']['title'],
+      profilePictureUrl: map['snippet']['thumbnails']['default']['url'],
+      subscriberCount: map['statistics']['subscriberCount'],
+      videoCount: map['statistics']['videoCount'],
+      uploadPlaylistId: map['contentDetails']['relatedPlaylists']['uploads'],
+    );
+  }
+}
+
+final List<Map<String, String>> channelIds = [
+  {
+    'id': 'UC6Dy0rQ6zDnQuHQ1EeErGUA',
+    'name': 'MarcusNG',
+  },
+  {
+    'id': 'UCm_-MeTSdYQjUIhS8VUR5NA',
+    'name': 'Menejer',
+  },
+];
